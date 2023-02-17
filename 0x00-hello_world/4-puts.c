0e9d6ff8eb66e7ef86413036fbce03ada3bcd888
YoussefSaad <youssefsaadjooe@gmail.com>
2023-02-16 16:34:24 -0800

    44444

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/*
+ *This program main function is to print that exact message
+ *
+ *
+ */
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+     puts("\"Programming is like building a multilingual puzzle");
+     return (0);
 
 }
