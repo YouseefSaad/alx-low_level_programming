4611f83604f6eabd42815fd4877e0b9997217385
YoussefSaad <youssefsaadjooe@gmail.com>
2023-02-17 17:43:37 -0800

    6666666666666666666666666666666666666666666666666666666666666

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-        int a;
+	int num;
 
-        for (a = 0; a < 10; a++)
-        
-                putchar((a % 10) + '0');
-        
-        putchar('\n');
-        return (0);
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
 }
